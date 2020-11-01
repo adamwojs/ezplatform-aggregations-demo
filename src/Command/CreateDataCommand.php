e180fc629d3e01ad6584979c3cf4c97259d75c04
Adam Wójs <adam@wojs.pl>
2020-11-01 10:03:00 +0100

    Tag cloud example

--- src/Command/CreateDataCommand.php
+++ src/Command/CreateDataCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use eZ\Publish\API\Repository\Exceptions\NotFoundException;
 use eZ\Publish\API\Repository\Repository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -28,8 +29,73 @@ final class CreateDataCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->repository->getPermissionResolver()->setCurrentUserReference(
+            $this->repository->getUserService()->loadUserByLogin('admin')
+        );
 
+        $this->createDataForTagCloudExample();
 
         return self::SUCCESS;
+    }
+
+    private function createDataForTagCloudExample(): void
+    {
+        $contentTypeService = $this->repository->getContentTypeService();
+
+        try {
+            $contentType = $contentTypeService->loadContentTypeByIdentifier('code_snippet');
+        } catch (NotFoundException $e) {
+            $contentTypeCreateStruct = $contentTypeService->newContentTypeCreateStruct('code_snippet');
+            $contentTypeCreateStruct->mainLanguageCode = 'eng-GB';
+            $contentTypeCreateStruct->names = ['eng-GB' => 'Code snippet'];
+            $contentTypeCreateStruct->creatorId = 14;
+            $contentTypeCreateStruct->creationDate = new \DateTime();
+
+            $fieldCreate = $contentTypeService->newFieldDefinitionCreateStruct('title', 'ezstring');
+            $fieldCreate->names = ['eng-GB' => 'title'];
+            $fieldCreate->fieldGroup = 'main';
+            $fieldCreate->position = 1;
+
+            $contentTypeCreateStruct->addFieldDefinition($fieldCreate);
+
+            $fieldCreate = $contentTypeService->newFieldDefinitionCreateStruct('keywords', 'ezkeyword');
+            $fieldCreate->names = ['eng-GB' => 'Keywords'];
+            $fieldCreate->fieldGroup = 'main';
+            $fieldCreate->position = 2;
+
+            $contentTypeCreateStruct->addFieldDefinition($fieldCreate);
+
+            $contentGroup = $contentTypeService->loadContentTypeGroupByIdentifier('Content');
+            $contentTypeDraft = $contentTypeService->createContentType($contentTypeCreateStruct, [$contentGroup]);
+            $contentTypeService->publishContentTypeDraft($contentTypeDraft);
+            $contentType = $contentTypeService->loadContentType($contentTypeDraft->id);
+        }
+
+        $contentService = $this->repository->getContentService();
+        $locationService = $this->repository->getLocationService();
+
+        $tags = [
+            'PHP', 'Java', 'XML', 'JavaScript', 'Ruby', 'Python', 'C', 'Go', 'Dart', 'TypeScript', 'Prolog',
+            'Pascal', 'HTML', 'SVG', 'C#', 'C++', 'Cobol', 'Brain F**k', 'Whitespace', 'Kotlin'
+        ];
+
+        for ($i = 1; $i <= 10; $i++) {
+            $contentCreateStruct = $contentService->newContentCreateStruct($contentType, 'eng-GB');
+            $contentCreateStruct->alwaysAvailable = false;
+            $contentCreateStruct->setField('title', 'Code snippet ' . $i);
+
+            $contentCreateStruct->setField('keywords', array_map(function ($idx) use ($tags) {
+                return $tags[$idx];
+            }, array_rand($tags, rand(3, count($tags)))));
+
+            $draft = $contentService->createContent(
+                $contentCreateStruct,
+                [
+                    $locationService->newLocationCreateStruct(2),
+                ]
+            );
+
+            $contentService->publishVersion($draft->getVersionInfo());
+        }
     }
 }
---
+++ src/Controller/TagCloudController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use eZ\Publish\API\Repository\SearchService;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\Field\KeywordTermAggregation;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\ContentTypeIdentifier;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class TagCloudController extends AbstractController
+{
+    /** @var \eZ\Publish\API\Repository\SearchService */
+    private $searchService;
+
+    public function __construct(SearchService $searchService)
+    {
+        $this->searchService = $searchService;
+    }
+
+    /**
+     * @Route("/tag_cloud", name="tag_cloud")
+     */
+    public function renderAction(): Response
+    {
+        $aggregation = new KeywordTermAggregation('tags', 'code_snippet', 'keywords');
+        $aggregation->setLimit(100);
+        $aggregation->setMinCount(3);
+
+        $query = new Query();
+        $query->limit = 0;
+        $query->filter = new ContentTypeIdentifier('code_snippet');
+        $query->aggregations[] = $aggregation;
+
+        $results = $this->searchService->findContent($query);
+
+        return $this->render('@ezdesign/tag_cloud/show.html.twig', [
+            'tags' => $results->aggregations->get('tags'),
+        ]);
+    }
+}
