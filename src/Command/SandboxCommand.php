5c00359b38605b3d1da392b104b067a6f05d6667
Adam Wójs <adam.wojs@ez.no>
2020-12-08 22:00:43 +0100

    Improved sandbox command example

--- src/Command/SandboxCommand.php
+++ src/Command/SandboxCommand.php
@@ -6,8 +6,13 @@ namespace App\Command;
 
 use eZ\Publish\API\Repository\Repository;
 use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\ContentTypeTermAggregation;
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\LanguageTermAggregation;
+use eZ\Publish\API\Repository\Values\Content\Search\AggregationResult\TermAggregationResult;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\OutputStyle;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 final class SandboxCommand extends AbstractRepositoryCommand
 {
@@ -18,22 +23,54 @@ final class SandboxCommand extends AbstractRepositoryCommand
 
     protected function doExecute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+
         $searchService = $this->repository->getSearchService();
 
         $query = new Query();
-        // Add your aggregation here:
-        // $query->aggregations[] = ...
+        // Attach your aggregation here:
+        $query->aggregations[] = new ContentTypeTermAggregation('content_by_type');
+        // Multiple aggregations could be computed in one query
+        $query->aggregations[] = new LanguageTermAggregation('content_by_language');
 
-        // We don't need search hits
-        $query->limit = 0;
+        // Aggregation results respects filter/query ...
+        // $query->filter = new ContentTypeIdentifier(['folder', 'article', 'review']);
+
+        // ... but ignores limit and offset
+        // $query->offset = 0;
+        // $query->limit = 0;
 
         $results = $searchService->findContent($query);
 
-        if (!$results->aggregations->isEmpty()) {
-            // Display result of the first aggregation
-            dump($results->aggregations->first());
-        }
+        // Aggregation results are accessible via $results->aggregations property
+
+        // Display result of the first aggregation
+        // $this->renderContentTypeTermAggregation($io, $results->aggregations->first());
+        // Access results by aggregation name
+        // $this->renderLanguageTermAggregation($io, $results->aggregations->get('content_by_language'));
 
         return self::SUCCESS;
+    }
+
+    private function renderContentTypeTermAggregation(OutputStyle $io, TermAggregationResult $result): void
+    {
+        $rows = [];
+        foreach ($result as $contentType => $count) {
+            /* @var \eZ\Publish\API\Repository\Values\ContentType\ContentType $contentType */
+            $rows[] = [$contentType->identifier, $count];
+        }
+
+        $io->table(['content type', 'count'], $rows);
+    }
+
+    private function renderLanguageTermAggregation(OutputStyle $io, TermAggregationResult $result): void
+    {
+        $rows = [];
+        foreach ($result as $language => $count) {
+            /* @var \eZ\Publish\API\Repository\Values\Content\Language $language */
+            $rows[] = [$language->name, $count];
+        }
+
+        $io->table(['content type', 'count'], $rows);
     }
 }
